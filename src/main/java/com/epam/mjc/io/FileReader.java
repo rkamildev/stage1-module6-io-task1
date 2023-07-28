611828a6a722fac97d4536e1da20bdbf3ca076b6
rkamildev <rkamil.int@gmail.com>
2023-07-28 16:26:01 +0500

    File Prac

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,31 @@
 package com.epam.mjc.io;
 
-import java.io.File;
-
+import java.io.*;
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Profile profile = new Profile();
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file)))) {
+            String line = reader.readLine();
+            while (line != null) {
+                String[] attr = line.split(":\\s+");
+                if (attr[0].equals("Name"))
+                    profile.setName(attr[1]);
+                if (attr[0].equals("Age"))
+                    profile.setAge(Integer.valueOf(attr[1]));
+                if (attr[0].equals("Email"))
+                    profile.setEmail(attr[1]);
+                if (attr[0].equals("Phone"))
+                    profile.setPhone(Long.valueOf(attr[1]));
+                line = reader.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return profile;
     }
 }
